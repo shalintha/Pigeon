13fcebbfd9050a1d3ae098e14d582beecbf4068a
SHalintHa <subhashana011@gmail.com>
2018-10-06 23:56:42 +0530

    Login users into their accounts

--- app/src/main/java/com/example/subhashana/pigeonn/LoginActivity.java
+++ app/src/main/java/com/example/subhashana/pigeonn/LoginActivity.java
@@ -1,22 +1,107 @@
 package com.example.subhashana.pigeonn;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private FirebaseAuth mAuth;
+
+    private Button LoginButton;
+    private EditText LoginEmail;
+    private EditText LoginPassword;
+
+    private ProgressDialog loadingbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
 
         mToolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Sign In");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        LoginButton = (Button) findViewById(R.id.login_button);
+        LoginEmail = (EditText) findViewById(R.id.login_email);
+        LoginPassword = (EditText) findViewById(R.id.login_password);
+
+        loadingbar = new ProgressDialog(this);
+
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = LoginEmail.getText().toString();
+                String password = LoginPassword.getText().toString();
+
+                LoginUserAccount(email, password);
+
+            }
+        });
+
+    }
+
+    private void LoginUserAccount(String email, String password) {
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(LoginActivity.this, "Please enter your email",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(LoginActivity.this, "Please enter your password",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+
+            loadingbar.setTitle("Login Account");
+            loadingbar.setMessage("Please wait!");
+            loadingbar.show();
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+
+                                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+
+                            }
+
+                            else{
+                                Toast.makeText(LoginActivity.this, "Please enter valid email and password",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            loadingbar.dismiss();
+                        }
+                    });
+        }
     }
 }
