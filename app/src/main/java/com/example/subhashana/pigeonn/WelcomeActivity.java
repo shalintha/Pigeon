61afdbb15e576d8321630b85446fd5caf2b6fa3a
SHalintHa <subhashana011@gmail.com>
2018-10-06 19:12:53 +0530

    Add validations on MainActivity and create StartPAgeActivity

--- app/src/main/java/com/example/subhashana/pigeonn/WelcomeActivity.java
+++ app/src/main/java/com/example/subhashana/pigeonn/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.subhashana.pigeonn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,34 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(2000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+
+                    Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
