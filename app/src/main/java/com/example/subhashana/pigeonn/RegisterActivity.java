697f2320c001696f0725f531dd1fee43eecb5aba
SHalintHa <subhashana011@gmail.com>
2018-10-06 21:24:53 +0530

    Register new users

--- app/src/main/java/com/example/subhashana/pigeonn/RegisterActivity.java
+++ app/src/main/java/com/example/subhashana/pigeonn/RegisterActivity.java
@@ -1,22 +1,113 @@
 package com.example.subhashana.pigeonn;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
+    private FirebaseAuth mAuth;
 
+    private Toolbar mToolbar;
+    private ProgressDialog loadingbar;
+
+    private EditText RegisterUserName;
+    private EditText RegisterUserEmail;
+    private EditText RegisterUserPassword;
+    private Button CreateAccountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
 
         mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Sign Up");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        RegisterUserName = (EditText) findViewById(R.id.register_name);
+        RegisterUserEmail = (EditText) findViewById(R.id.register_email);
+        RegisterUserPassword = (EditText) findViewById(R.id.register_password);
+        CreateAccountButton = (Button) findViewById(R.id.create_account_button);
+        loadingbar = new ProgressDialog(this);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = RegisterUserName.getText().toString();
+                String email = RegisterUserEmail.getText().toString();
+                String password = RegisterUserPassword.getText().toString();
+
+
+                RegisterAccount(name, email, password);
+
+            }
+        });
+
+    }
+
+    private void RegisterAccount(String name, String email, String password) {
+
+        if (TextUtils.isEmpty(name)){
+            Toast.makeText(RegisterActivity.this, "Please enter your name",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(RegisterActivity.this, "Please enter your email",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(RegisterActivity.this, "Please enter your password",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        else{
+
+            loadingbar.setTitle("Creating Account");
+            loadingbar.setMessage("Please wait!");
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+                            }
+
+                            else{
+
+                                Toast.makeText(RegisterActivity.this, "Error Occured, Try again!",
+                                        Toast.LENGTH_LONG).show();
+
+                            }
+                            loadingbar.dismiss();
+                        }
+                    });
+
+        }
+
     }
 }
