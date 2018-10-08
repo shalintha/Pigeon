f0ed9cfcad8b4542ebcef76236d06fbb72f8543e
SHalintHa <subhashana011@gmail.com>
2018-10-08 21:35:32 +0530

    Modified FirebaseMessagingService. Create a function for click on a notification

--- app/src/main/java/com/example/subhashana/pigeonn/FirebaseMassagingService.java
+++ app/src/main/java/com/example/subhashana/pigeonn/FirebaseMassagingService.java
@@ -1,6 +1,8 @@
 package com.example.subhashana.pigeonn;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -17,12 +19,35 @@ public class FirebaseMassagingService extends FirebaseMessagingService {
         String notification_body = remoteMessage.getNotification().getBody();
 
 
+
+        String click_action = remoteMessage.getNotification().getClickAction();
+
+        String from_sender_id = remoteMessage.getData().get("from_sender_id").toString();
+
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.pigeonn_icon_round)
                 .setContentTitle(notification_title)
                 .setContentText(notification_body)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        Intent resultInent = new Intent(click_action);
+        resultInent.putExtra("visit_user_id", from_sender_id);
+
+
+
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultInent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
 
 
         int mNotificationId = (int) System.currentTimeMillis();
