980ac1dc258ab7d2866e870e464437639495fb61
S H A L I N T H A <26096410+SHalintHa@users.noreply.github.com>
2019-08-08 01:21:31 +0530

    Add Microsoft App Center's SDK

--- app/src/main/java/com/example/subhashana/pigeonn/WelcomeActivity.java
+++ app/src/main/java/com/example/subhashana/pigeonn/WelcomeActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        AppCenter.start(getApplication(), "da90eb32-96db-453a-b01c-2a14fe8740ae",
+                Analytics.class, Crashes.class);
 
 
         Thread thread = new Thread(){
